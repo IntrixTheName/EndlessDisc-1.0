b86deb38f56c6c33ffa9cadef82718a8b7d6367c
IntrixTheName <intrixthename@gmail.com>
2024-01-28 15:25:34 -0500

    Toolbar troubleshooting

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import Toolbar from './components/Toolbar.js';
 function App() {
   return (
     <div className="App">
-      <Toolbar />
       <header className="App-header">
+        <Toolbar />
         <p>
           Edit <code>src/App.js</code> and save to reload. Hello Bradley.
         </p>
       </header>
       <body>
-        <h1>Sample text to make sure the page isn't broken...</h1>
+        <p>Sample text to make sure the page isn't broken...</p>
       </body>
     </div>
   );
