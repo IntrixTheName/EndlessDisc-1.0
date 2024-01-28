c38c43bf3684a9ca00743b398c295e287ca21879
IntrixTheName <intrixthename@gmail.com>
2024-01-28 16:01:20 -0500

    Toolbar troubleshooting

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -5,17 +5,19 @@ import 'Toolbar.css';
 import banner_logo from '../assets/logo-notext.svg';
 
 function ToolbarItem(props) {
-  return <button class="button">{props.text}</button>
+  return <li className="button">{props.text}</li>
 }
 
 function Toolbar() {
   return (
     <div className="toolbar">
       <img src={banner_logo} alt="Endless Disc" width="192px" height="108px" />
-      <ToolbarItem text="Home" />
-      <ToolbarItem text="Library" />
-      <ToolbarItem text="Import" />
-      <ToolbarItem text="Export" />
+      <ul className="tabs">
+        <ToolbarItem text="Home" />
+        <ToolbarItem text="Library" />
+        <ToolbarItem text="Import" />
+        <ToolbarItem text="Export" />
+      </ul>
     </div>
   );
 };
