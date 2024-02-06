f0fd5ebe65b4e81ddff9d057c58bf9a310765c77
IntrixTheName <intrixthename@gmail.com>
2024-02-06 03:44:36 +0000

    Reintroduced fonts

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,5 +1,5 @@
 import { Outlet, Link } from "react-router-dom";
-import Toolbar from "./Toolbar";
+//import Toolbar from "./Toolbar";
 import './Toolbar.css';
 import logo from '../assets/logo-notext.svg';
 
